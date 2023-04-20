d0064d2d9b5fc697f9372d2487a3093fd47a46a0
LendorBrr <96260311+LendorBrr@users.noreply.github.com>
2023-04-20 21:33:44 +0300

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -7,20 +7,20 @@ from google.cloud.speech_v1p1beta1 import types
 
 # Transcribe audio using Google Speech-to-Text API
 def transcribe_audio(gcs_uri):
-    credentials = service_account.Credentials.from_service_account_file("/users/mv/googleconsole/.json")
+    credentials = service_account.Credentials.from_service_account_file("/path/to/json/file.json")
     client = speech.SpeechClient(credentials=credentials)
 
     audio = speech.RecognitionAudio(uri=gcs_uri)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.MP3,  # Assuming the file is in MP3 format
-        sample_rate_hertz=16000,
+        sample_rate_hertz=44100,
         language_code="en-US",
     )
 
     response = client.long_running_recognize(config=config, audio=audio)
 
     print("Waiting for operation to complete...")
-    response = response.result(timeout=90)
+    response = response.result(timeout=300)
 
     transcription = ""
     for result in response.results:
@@ -31,14 +31,14 @@ def transcribe_audio(gcs_uri):
 # Summarize text using ChatGPT-4 API
 
 def generate_summary(text):
-    openai_api_key = "XXX"
+    openai_api_key = "YOUR-API-KEY"
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {openai_api_key}"
     }
 
     data = {
-        "prompt": f"Please provide a summary of the following text in 2 bullet points:\n\n{text}",
+        "prompt": f"Please provide a summary of the following text in 5 bullet points:\n\n{text}",
         "max_tokens": 150,
         "n": 1,
         "stop": None,
@@ -64,8 +64,8 @@ def send_email_summary(email_addresses, summary, edited_summary=None):
     if edited_summary is not None:
         summary = edited_summary
 
-    email_sender = "transcriber@alprodarbai.lt"
-    email_password = "Neilaqgt1$"
+    email_sender = "EMAIL@EMAIL.COM"
+    email_password = "Donkey123"
 
     msg = MIMEMultipart()
     msg["From"] = email_sender
@@ -76,12 +76,9 @@ def send_email_summary(email_addresses, summary, edited_summary=None):
     msg.attach(body)
 
     try:
-        with smtplib.SMTP_SSL("gnldm1071.siteground.biz", 465) as server:
+        with smtplib.SMTP_SSL("XXXX.siteground.biz", 465) as server:
             server.login(email_sender, email_password)
             server.sendmail(email_sender, email_addresses, msg.as_string())
             print("Email sent successfully!")
     except Exception as e:
         print("Error while sending email:", e)
-
-
-
